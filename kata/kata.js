113b11012cfb41b6813e73b9c4b82260bd925166
Dmytro Cheromushev <dmytro.cheromushev@gmail.com>
2021-01-20 21:04:08 +0200

    Drag-and-drop v.1
    1. Added drag-and-drop attributes to child list component elements;
    2. Implemented drag-and-drop event handlers in the child component.

--- kata/kata.js
+++ kata/kata.js
@@ -5,7 +5,7 @@ export default class Kata extends LightningElement {
     itemsValueMediaFragment = "List Value";
 
     get firstItemsArray() { 
-        let valueOrdinalPrefix = "First";
+        const valueOrdinalPrefix = "First";
         return [
             {guid: "001", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
             {guid: "010", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
@@ -13,7 +13,7 @@ export default class Kata extends LightningElement {
         ];
     } 
     get secondItemsArray() {
-        let valueOrdinalPrefix = "Second";
+        const valueOrdinalPrefix = "Second";
         return [
             {guid: "100", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
             {guid: "101", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
--- listOfDragAndDroppableItems/listOfDragAndDroppableItems.js
+++ listOfDragAndDroppableItems/listOfDragAndDroppableItems.js
@@ -8,4 +8,22 @@ export default class ListOfDragAndDroppableItems extends LightningElement {
     @api
     items;
 
+    handleDragStart(event) { 
+        event.dataTransfer.setData("itemId", event.target.dataset.item); 
+        event.dataTransfer.setData("itemValue", event.target.innerText); 
+    } 
+    handleDragOver(event) { 
+        event.preventDefault(); 
+    } 
+    handleDrop(event) {
+
+        event.preventDefault();
+
+        const itemId = event.dataTransfer.getData("itemId"); 
+        console.log("handleDrop() is being executed");
+        this.items.push({guid: itemId, value: event.dataTransfer.getData("itemValue")}); 
+        console.log("items: " + JSON.stringify(this.items));
+
+    }
+
 }
