9c3d136c19790f6d1b6fb3e1d0424f8f31e180bf
Dmytro Cheromushev <dmytro.cheromushev@gmail.com>
2021-01-22 09:02:01 +0200

    Drag-and-drop Message Channel
    1. Created Drag-and-drop Message Channel;
    2. Added a message context for drag-and-drop to child list components;
    3. Implemented pub-sub behaviour for child list components;
    4. Added public property of GUID to child list components.

--- kata/kata.js
+++ kata/kata.js
@@ -7,17 +7,17 @@ export default class Kata extends LightningElement {
     get firstItemsArray() { 
         const valueOrdinalPrefix = "First";
         return [
-            {guid: "001", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
-            {guid: "010", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
-            {guid: "011", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 3}
+            {guid: "1", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
+            {guid: "2", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
+            {guid: "3", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 3}
         ];
     } 
     get secondItemsArray() {
         const valueOrdinalPrefix = "Second";
         return [
-            {guid: "100", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
-            {guid: "101", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
-            {guid: "110", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 3}
+            {guid: "4", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 1}, 
+            {guid: "5", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 2}, 
+            {guid: "6", value: valueOrdinalPrefix + "\u0020" + this.itemsValueMediaFragment + "\u0020" + 3}
         ];
     }
 
--- listOfDragAndDroppableItems/listOfDragAndDroppableItems.js
+++ listOfDragAndDroppableItems/listOfDragAndDroppableItems.js
@@ -1,29 +1,86 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+
+import { createMessageContext, releaseMessageContext, publish, subscribe } from 'lightning/messageService'; 
+import DragAndDropMessageChannel from "@salesforce/messageChannel/DragAndDropMessageChannel__c";
 
 export default class ListOfDragAndDroppableItems extends LightningElement {
+
+    messageContext = createMessageContext(); 
+    subscription = null;
+
+    dragAndDroppedItemGuid; 
+    dragAndDroppedItemValue;
+
+    constructor() {
+
+        super();
+
+        if (this.subscription) { 
+            return; 
+        } 
+        this.subscription = subscribe(this.messageContext, DragAndDropMessageChannel, (dragAndDropMessage) => {
+
+            if (
+                !dragAndDropMessage.isItemUpcomingToBeDropped && 
+                this.guid !== dragAndDropMessage.listGuid // a little duck tape here
+            ) {
+
+                this.listElements = this.listElements.filter(({guid}) => guid != dragAndDropMessage.itemGuid);
+
+            } 
+            else {
+                this.dragAndDroppedItemGuid = dragAndDropMessage.itemGuid; 
+                this.dragAndDroppedItemValue = dragAndDropMessage.itemValue; 
+            } 
+        }); 
+    }
+
+    @api
+    guid;
 
     @api
     label;
 
+    @track
+    listElements;
     @api
-    items;
+    get items() {
+        return this.listElements;
+    }
+    set items(value) {
+        this.listElements = value;
+    }
 
     handleDragStart(event) { 
-        event.dataTransfer.setData("itemId", event.target.dataset.item); 
-        event.dataTransfer.setData("itemValue", event.target.innerText); 
+        const dragAndDropMessage = { 
+            itemGuid: event.target.dataset.item, 
+            itemValue: event.target.innerText, 
+            isItemUpcomingToBeDropped: true
+        }; 
+        publish(this.messageContext, DragAndDropMessageChannel, dragAndDropMessage); 
     } 
     handleDragOver(event) { 
         event.preventDefault(); 
     } 
     handleDrop(event) {
+        console.log("handleDrop() is being executed"); 
+        event.preventDefault(); 
+        console.log("listElements before an adding of a dropped item: " + JSON.stringify(this.listElements)); 
+        this.listElements = [...this.listElements, {guid: this.dragAndDroppedItemGuid, value: this.dragAndDroppedItemValue}];
+        console.log("listElements after the adding of the dropped item: " + JSON.stringify(this.listElements)); 
+        const dragAndDropMessage = { 
+            listGuid: this.guid, 
+            itemGuid: this.dragAndDroppedItemGuid, 
+            itemValue: null, 
+            isItemUpcomingToBeDropped: false 
+        }; 
+        console.log("dragAndDropMessage in drop handling: " + JSON.stringify(dragAndDropMessage)); 
+        publish(this.messageContext, DragAndDropMessageChannel, dragAndDropMessage);
 
-        event.preventDefault();
+    }
 
-        const itemId = event.dataTransfer.getData("itemId"); 
-        console.log("handleDrop() is being executed");
-        this.items.push({guid: itemId, value: event.dataTransfer.getData("itemValue")}); 
-        console.log("items: " + JSON.stringify(this.items));
-
+    disconnectedCallback() { 
+        releaseMessageContext(this.messageContext); 
     }
 
 }
